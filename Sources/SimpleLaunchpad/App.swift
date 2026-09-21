acfaa80edc194bfd9847a4df848e90601ebea458
Gabriel Kanev <contact@mrgkanev.eu>
2026-09-21 14:29:02 +0300

    chore: scaffold SimpleLaunchpad SPM executable target

---
+++ Sources/SimpleLaunchpad/App.swift
@@ -0,0 +1,14 @@
+import AppKit
+
+@main
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    static func main() {
+        let app = NSApplication.shared
+        let delegate = AppDelegate()
+        app.delegate = delegate
+        app.setActivationPolicy(.accessory)
+        app.run()
+    }
+
+    func applicationDidFinishLaunching(_ notification: Notification) {}
+}
