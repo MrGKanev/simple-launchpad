e1646bf0451c1564a7254d5065fe3af1dae58d70
Gabriel Kanev <contact@mrgkanev.eu>
2026-09-21 15:26:03 +0300

    feat: add .app bundle packaging script

---
+++ Scripts/build-app-bundle.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+APP_NAME="Simple Launchpad"
+BUILD_DIR=".build/release"
+APP_DIR="$BUILD_DIR/$APP_NAME.app"
+
+swift build -c release
+
+rm -rf "$APP_DIR"
+mkdir -p "$APP_DIR/Contents/MacOS"
+mkdir -p "$APP_DIR/Contents/Resources"
+cp "$BUILD_DIR/SimpleLaunchpad" "$APP_DIR/Contents/MacOS/$APP_NAME"
+cp "$(dirname "$0")/../Sources/SimpleLaunchpad/Resources/Info.plist" "$APP_DIR/Contents/Info.plist"
+
+echo "Built $APP_DIR"
